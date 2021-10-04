8fa481f98f098b93e3580a1255d5424ba94ed7e5
Melker Stålnacke <melkerstalnacke0@gmail.com>
2021-10-04 09:08:33 +0200

    426

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Assignments
     {
         static void Main(string[] args)
         {
-            ProgSharpK4.Uppgift424(); 
+            ProgSharpK4.Uppgift426(); 
            
             Console.ReadKey(); 
         }  
