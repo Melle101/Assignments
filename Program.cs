17fe6232ace98d134e0e914b03128c5d2b16b144
Melker Stålnacke <melkerstalnacke0@gmail.com>
2021-11-28 03:20:51 +0100

    fds

--- Program.cs
+++ Program.cs
@@ -13,9 +13,44 @@ namespace Assignments
     {
         static void Main(string[] args)
         {
-            Other.UppgiftA();
-           
-            Console.ReadKey(); 
+            
+
+            int?[,] SolvedBoard = Sudoku.SudokuFillTest();
+
+            for (int s = 0; s < 40; s++) //Remove upto 40 random numbers, might overlap remove
+            {
+                int i = new Random().Next(0, 9);
+                int j = new Random().Next(0, 9);
+                SolvedBoard[i, j] = null;
+            }
+
+            for (int i = 0; i < 9; i++) //Print board with removed cells
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (SolvedBoard[i, j] == null) Console.Write(".");
+                    else Console.Write(SolvedBoard[i, j].ToString());
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n \n \n \n");
+            
+
+            SolvedBoard = Sudoku.SudokuSolver(SolvedBoard);
+            for (int i = 0; i < 9; i++) //Print final board
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (SolvedBoard[i, j] == null) Console.Write(".");
+                    else Console.Write(SolvedBoard[i, j].ToString());
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+
         }  
     }
 }
